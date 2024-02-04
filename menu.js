ac4f4232266db8cbcc0f43130a921a64b4a6fe11
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2024-02-04 04:34:16 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,27 @@
+function filterItems(category) {
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => {
+        item.style.display = 'none';
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionner tous les boutons
+    const buttons = document.querySelectorAll('.btn__projects');
+  
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+          if (!this.classList.contains('btn__active')) {
+            // Retirer la classe "active" de tous les boutons
+            buttons.forEach(function (btn) {
+              btn.classList.remove('btn__active');
+            });
+    
+            // Ajouter la classe "active" au bouton actuel
+            this.classList.add('btn__active');
+          }
+        });
+      });
+    });
