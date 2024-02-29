16c1e17e373f9fbda64329fb7ee305640f715aa2
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2024-02-29 22:06:16 +0100

    Add files via upload

--- js/cookie-banner.js
+++ js/cookie-banner.js
@@ -1,3 +1,40 @@
+// Fonction pour définir un cookie
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Fonction pour récupérer la valeur d'un cookie
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    
+    for (const cookie of ca) {
+        let c = cookie.trim(); // Utilisez trim() pour supprimer les espaces au début et à la fin
+        if (c.startsWith(nameEQ)) return c.substring(nameEQ.length, c.length);
+    }
+
+    return null;
+}
+
+// Fonction pour cacher la bannière de consentement
+function hideCookieBanner() {
+    document.getElementById('cookieBanner').style.display = 'none';
+}
+
+// Vérification du cookie d'acceptation des cookies au chargement de la page
+window.onload = function () {
+    const cookiesAccepted = getCookie('cookies_accepted');
+    if (!cookiesAccepted) {
+        document.getElementById('cookieBanner').style.display = 'block';
+    }
+};
+
 // Fonction pour cacher la bannière de consentement et définir un cookie d'acceptation
 function acceptCookies() {
     hideCookieBanner();
@@ -7,20 +44,4 @@ function acceptCookies() {
 // Fonction pour cacher la bannière de consentement sans définir de cookie
 function rejectCookies() {
     hideCookieBanner();
-}
-
-// Fonction pour définir un cookie
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-// Fonction pour cacher la bannière de consentement
-function hideCookieBanner() {
-    document.getElementById('cookieBanner').style.display = 'none';
 }
--- js/email.js
+++ js/email.js
@@ -1,40 +1,41 @@
+// Fonction pour générer des liens e-mail dynamiques
 function generateEmailLinks() {
-    var user = "emanuela.palazzo.lacanfora";
-    var domain = "gmail.com";
-    var subject = "MON ART";
-    
+    const user = "emanuela.palazzo.lacanfora";
+    const domain = "gmail.com";
+    const subject = "MON ART";
+
     // Génération dynamique de l'adresse e-mail avec une méthode complexe
-    var dynamicEmail = generateDynamicEmail(user, domain);
+    const dynamicEmail = generateDynamicEmail(user, domain);
 
     // Sélectionnez tous les éléments avec la classe 'emailLink'
-    var emailLinks = document.querySelectorAll('.emailLink');
+    const emailLinks = document.querySelectorAll('.emailLink');
 
     // Appliquez le lien 'mailto' à tous les éléments
-    emailLinks.forEach(function(link) {
-      var mailtoLink = 'mailto:' + dynamicEmail + '?subject=' + encodeURIComponent(subject);
-      link.setAttribute("href", mailtoLink);
+    emailLinks.forEach(link => {
+        const mailtoLink = `mailto:${dynamicEmail}?subject=${encodeURIComponent(subject)}`;
+        link.setAttribute("href", mailtoLink);
     });
-  }
+}
 
-  // Fonction pour générer dynamiquement l'adresse e-mail avec une méthode complexe
-  function generateDynamicEmail(user, domain) {
+// Fonction pour générer dynamiquement l'adresse e-mail avec une méthode complexe
+function generateDynamicEmail(user, domain) {
     // Mettez en œuvre votre propre logique de génération ici, par exemple, mélangez les caractères, ajoutez des éléments supplémentaires, etc.
-    var shuffledUser = shuffleString(user);
-    var shuffledDomain = shuffleString(domain);
-    
-    return shuffledUser + "@" + shuffledDomain;
-  }
+    const shuffledUser = shuffleString(user);
+    const shuffledDomain = shuffleString(domain);
 
-  // Fonction pour mélanger les caractères d'une chaîne de manière aléatoire
-  function shuffleString(str) {
-    var arr = str.split('');
-    for (var i = arr.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+    return `${shuffledUser}@${shuffledDomain}`;
+}
+
+// Fonction pour mélanger les caractères d'une chaîne de manière aléatoire
+function shuffleString(str) {
+    const arr = str.split('');
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     return arr.join('');
-  }
+}
 
-  window.onload = generateEmailLinks;
+window.onload = generateEmailLinks;
