ee3b2d16a89d8c6d1d28963c21642df5df62ce39
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2024-03-01 13:46:40 +0100

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,21 +1,63 @@
-// Fonction pour définir un cookie
-function setCookie(name, value, days) {
+          // Vérification du cookie d'acceptation des cookies au chargement de la page
+window.onload = function () {
+    const cookiesAccepted = getCookie('cookies_accepted');
+    console.log("Cookies acceptés :", cookiesAccepted);
+
+    if (!cookiesAccepted || cookiesAccepted !== 'true') {
+        console.log("Afficher la bannière");
+        document.getElementById('cookieBanner').style.display = 'block';
+    } else {
+        console.log("Pas besoin d'afficher la bannière, les cookies sont acceptés.");
+        document.getElementById('cookieBanner').style.display = 'none';
+    }
+};
+
+// Accepter les cookies pour le chemin /fr/
+function acceptCookiesFR() {
+    acceptCookies('/fr/');
+}
+
+// Accepter les cookies pour le chemin /en/
+function acceptCookiesEN() {
+    acceptCookies('/en/');
+}
+
+// Accepter les cookies pour le chemin /it/
+function acceptCookiesIT() {
+    acceptCookies('/it/');
+}
+
+// Fonction pour cacher la bannière de consentement et définir un cookie d'acceptation
+function acceptCookies(lang) {
+    hideCookieBanner();
+
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000)); // 30 jours d'expiration
+
+    setCustomCookie('cookies_accepted', 'true', 30, lang);
+    console.log("Cookies acceptés :", getCookie('cookies_accepted'));
+    console.log("Date d'expiration du cookie :", expirationDate.toUTCString());
+}
+
+// Fonction pour définir un cookie avec des options personnalisées
+function setCustomCookie(name, value, days, path) {
     let expires = "";
     if (days) {
         const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + value + expires + "; path=/";
+
+    document.cookie = name + "=" + value + expires + "; path=" + path + "; secure";
 }
 
 // Fonction pour récupérer la valeur d'un cookie
 function getCookie(name) {
     const nameEQ = name + "=";
     const ca = document.cookie.split(';');
-    
+
     for (const cookie of ca) {
-        let c = cookie.trim(); // Utilisez trim() pour supprimer les espaces au début et à la fin
+        let c = cookie.trim();
         if (c.startsWith(nameEQ)) return c.substring(nameEQ.length, c.length);
     }
 
@@ -25,20 +67,6 @@ function getCookie(name) {
 // Fonction pour cacher la bannière de consentement
 function hideCookieBanner() {
     document.getElementById('cookieBanner').style.display = 'none';
-}
-
-// Vérification du cookie d'acceptation des cookies au chargement de la page
-window.onload = function () {
-    const cookiesAccepted = getCookie('cookies_accepted');
-    if (!cookiesAccepted) {
-        document.getElementById('cookieBanner').style.display = 'block';
-    }
-};
-
-// Fonction pour cacher la bannière de consentement et définir un cookie d'acceptation
-function acceptCookies() {
-    hideCookieBanner();
-    setCookie('cookies_accepted', 'true', 30); // Le cookie expire dans 30 jours
 }
 
 // Fonction pour cacher la bannière de consentement sans définir de cookie
