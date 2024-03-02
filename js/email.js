a6b83e5e2a46e04c80615945ae004f5cf2ccc3e8
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2024-03-02 07:02:42 +0100

    Update email.js

--- js/email.js
+++ js/email.js
@@ -11,53 +11,13 @@ function generateEmailLinks() {
     emailLinks.forEach(link => {
         const mailtoLink = `mailto:${dynamicEmail}?subject=${subject}`;
         link.setAttribute("href", mailtoLink);
-        link.addEventListener("click", (event) => handleEmailLinkClick(event, dynamicEmail));
+        link.textContent = dynamicEmail;
+        link.classList.add('protected-email');
     });
 }
 
 function generateDynamicEmail(user, domain) {
     return `${user}@${domain}`;
-}
-
-function handleEmailLinkClick(event, email) {
-    event.preventDefault();
-
-    // Créez un élément de checkbox
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = 'confirmationCheckbox';
-    checkbox.style.marginRight = '5px';  // Ajoutez une marge pour l'esthétique
-
-    // Créez une étiquette pour la checkbox
-    const label = document.createElement('label');
-    label.htmlFor = 'confirmationCheckbox';
-    label.appendChild(document.createTextNode('Je confirme que je ne suis pas un robot'));
-
-    // Créez un conteneur pour la checkbox et l'étiquette
-    const checkboxContainer = document.createElement('div');
-    checkboxContainer.appendChild(checkbox);
-    checkboxContainer.appendChild(label);
-
-    // Ajout de la checkbox à la boîte de dialogue de confirmation
-    const confirmation = confirm(`
-        Êtes-vous sûr de vouloir envoyer un e-mail à ${email}?
-        Cette action peut aider à prévenir le spam.
-    `);
-
-    // Si l'utilisateur confirme, ouvre le client de messagerie
-    if (confirmation) {
-        checkboxContainer.style.textAlign = 'center';  // Alignez la checkbox au centre
-        checkbox.checked = false;  // Assurez-vous qu'elle n'est pas cochée par défaut
-        checkbox.style.marginTop = '10px';  // Ajoutez une marge pour l'esthétique
-
-        // Ajoutez la checkbox à la boîte de dialogue
-        const checkboxResult = confirm(checkboxContainer);
-
-        // Si la checkbox est cochée, ouvre le client de messagerie
-        if (checkboxResult && checkbox.checked) {
-            window.location.href = event.currentTarget.getAttribute('href');
-        }
-    }
 }
 
 document.addEventListener("DOMContentLoaded", generateEmailLinks);
